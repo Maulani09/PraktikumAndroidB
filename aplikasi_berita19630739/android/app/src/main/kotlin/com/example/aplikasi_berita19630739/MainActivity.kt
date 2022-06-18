7fbe963cf832055ae0f72ffc392f9fc0d4232cd8
Maulani09 <55121001+Maulani09@users.noreply.github.com>
2022-06-18 08:38:43 -0700

    PraktikumAndroid-AkhmadMaulani

---
+++ aplikasi_berita19630739/android/app/src/main/kotlin/com/example/aplikasi_berita19630739/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.aplikasi_berita19630739
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ aplikasi_berita19630739/lib/common/config.dart
@@ -0,0 +1,4 @@
+final String baseUrl = 'https://newsapi.org/v2/';
+final String apiKey = '022fc09b1fe44d83b34ce969ff6a6e06';
+final String country = 'id';
+final int pageSize = 20;
